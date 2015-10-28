d6613cba9d51a441664b67465901406d4aa44d05
Kopplin <you@example.com>
2015-10-28 19:38:38 -0200

    update: refactoring in sass files

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,15 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
-// Task for building blog when something changed:
 // gulp.task('build', shell.task(['bundle exec jekyll build --watch --drafts']));
-// Or if you don't use bundle:
 gulp.task('build', shell.task(['jekyll build --watch --drafts']));
 
-// Task for serving blog with Browsersync
 gulp.task('serve', function () {
     browserSync.init({
         server: { baseDir: '_site/' },
         open: false
     });
-    // Reloads page when some of the already built files changed:
+
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
