9fe3b5fd70718d53f12d9415647e6e4d0bb4e96d
Sergio Kopplin <sergiokopplin@gmail.com>
2015-11-01 14:38:08 -0200

    update: esc button to close the menu + removing the artic scroll and using pure jquery

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,110 +1,17 @@
-/**
- * Main JS file for Casper behaviours
- */
-
-/* globals jQuery, document */
-(function($, sr, undefined) {
+(function() {
     "use strict";
 
-    var $document = $(document),
-
-        // debouncing function from John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        debounce = function(func, threshold, execAsap) {
-            var timeout;
-
-            return function debounced() {
-                var obj = this,
-                    args = arguments;
-
-                function delayed() {
-                    if (!execAsap) {
-                        func.apply(obj, args);
-                    }
-                    timeout = null;
-                }
-
-                if (timeout) {
-                    clearTimeout(timeout);
-                } else if (execAsap) {
-                    func.apply(obj, args);
-                }
-
-                timeout = setTimeout(delayed, threshold || 100);
-            };
-        };
-
-    $document.ready(function() {
-
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        function updateImageWidth() {
-            var $this = $(this),
-                contentWidth = $postContent.outerWidth(), // Width of the content
-                imageWidth = this.naturalWidth; // Original image resolution
-
-            if (imageWidth >= contentWidth) {
-                $this.addClass('full-img');
-            } else {
-                $this.removeClass('full-img');
-            }
-        }
-
-        var $img = $("img").on('load', updateImageWidth);
-
-        function casperFullImg() {
-            $img.each(updateImageWidth);
-        }
-
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
-
-        $(".main-header__scroll-down").arctic_scroll();
-        $(".post-header__scroll-down").arctic_scroll();
+    $(".main-header__scroll-down").on('click', function() {
+        $('html, body').animate({
+            scrollTop: $("#content").offset().top
+        }, 1000);
     });
 
-    // smartresize
-    jQuery.fn[sr] = function(fn) {
-        return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr);
-    };
-
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function(options) {
-
-        var defaults = {
-                elem: $(this),
-                speed: 500
-            },
-
-            allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function(event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({
-                    scrollTop: ($(this.hash).offset().top + toMove)
-                }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({
-                    scrollTop: toMove
-                }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({
-                    scrollTop: ($(this.hash).offset().top)
-                }, allOptions.speed);
-            }
-        });
-    };
+    $(".post-header__scroll-down").on('click', function() {
+        $('html, body').animate({
+            scrollTop: $("#content").offset().top
+        }, 1000);
+    });
 
     var colors = [
         ['#D89F87', '#46324A', '#132327', '#0C0207'],
@@ -135,7 +42,7 @@
 
     $('.menu-wrap').attr('style', 'background-image: url(' + triangle.png() + ')');
 
-})(jQuery, 'smartresize');
+})();
 
 function togglescroll() {
     $('body').on('touchstart', function(e) {
@@ -176,15 +83,13 @@ $(document).ready(function() {
 });
 
 // // PUSH ESC KEY TO EXIT
-// $(document).keydown(function(e) {
-//   if (e.keyCode == 27) {
-//     $(".mobile-nav").fadeOut(500);
-//     $(".hamburguer__top-menu").removeClass("hamburguer__top-animate");
-//     $("body").removeClass("noscroll");
-//     $(".hamburguer__mid-menu").removeClass("hamburguer__mid-animate");
-//     $(".hamburguer__bottom-menu").removeClass("hamburguer__bottom-animate");
-//   }
-// });
+$(document).keydown(function(e) {
+    if (e.keyCode == 27) {
+        $("#hamburguer__close").fadeOut('fast');
+        $("body").removeClass("show-menu");
+        $("#hamburguer__open").fadeIn('fast');
+    }
+});
 
 // Sticky Header
 $(window).scroll(function() {
