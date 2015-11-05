4e172e9a6c1b35a8ddec9770e17809c17795cb45
Sergio Kopplin <sergiokopplin@gmail.com>
2015-11-05 00:08:32 -0200

    update: refactoring js and removing jquery - step 3

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,51 +1,73 @@
 var Kopplin = {
 
-    colors: [
-        ['#D89F87', '#46324A', '#132327', '#0C0207'],
-        ['#2b2645', '#182733', '#3b4f5e', '#2c2b31'],
-        ['#3f0122', '#270313', '#011e29', '#004e59'],
-        ['#4a1b44', '#280325', '#012922', '#005934'],
-        ['#1e2639', '#2b2233', '#88354c'],
-        ['#3F3F3F', '#5C605D', '#87AD83', '#B1D8B6', '#97D897'],
-        ['#645188', '#886451', '#528881', '#000000'],
-        ['#1b85b8', '#5a5255', '#ae5a41', '#c3cb71'],
-        ['#f29421', '#434292', '#132449'],
-        ['#18392b', '#eeeeee']
-    ],
-
+    /**
+     * init
+     * @access public
+     * @desc Inícia o JS dentro da página
+     * @return {void}
+     */
     init: function() {
         "use strict";
 
-        console.log("init");
-
-        smoothScroll.init();
+        // Função para aplicar background com Trianglify
         this.insertBackground();
-        this.events();
 
-        console.log("fim // init");
+        // Chama função com os eventos da página
+        this.events();
     },
 
+    /**
+     * insertBackground
+     * @access public
+     * @desc Insere background com a API do Trianglify nos elementos especificados
+     * @return {void}
+     */
     insertBackground: function() {
         "use strict";
 
-        var randomColor = Math.floor(Math.random() * (this.colors.length))
-            , randomSize = Math.floor(Math.random() * 60) + 20
-            , randomVariance = Math.random()
-            , mainHeader = document.querySelector('.main-header')
-            , menuWrap = document.querySelector('.menu-wrap')
+        // Padrões de cores que serão usadas no Trianglify
+        var colors = [
+            ['#D89F87', '#46324A', '#132327', '#0C0207'],
+            ['#2b2645', '#182733', '#3b4f5e', '#2c2b31'],
+            ['#3f0122', '#270313', '#011e29', '#004e59'],
+            ['#4a1b44', '#280325', '#012922', '#005934'],
+            ['#1e2639', '#2b2233', '#88354c'],
+            ['#3F3F3F', '#5C605D', '#87AD83', '#B1D8B6', '#97D897'],
+            ['#645188', '#886451', '#528881', '#000000'],
+            ['#1b85b8', '#5a5255', '#ae5a41', '#c3cb71'],
+            ['#f29421', '#434292', '#132449'],
+            ['#18392b', '#eeeeee']
+        ];
 
-            , triangle = Trianglify({
-                width: window.innerWidth,
-                height: window.innerHeight,
-                variance: .7,
-                cell_size: randomSize,
-                x_colors: this.colors[randomColor]
-            });
+        // Gera as cores, tamanhos e variância a serem aplicados na API do Trianglify
+        var randomColor = Math.floor(Math.random() * (colors.length))
+        , randomSize = Math.floor(Math.random() * 60) + 20
+        , randomVariance = Math.random()
 
+        // Elementos que usarão o background com Trianglify
+        , mainHeader = document.querySelector('.main-header')
+        , menuWrap = document.querySelector('.menu-wrap')
+
+        // Gera o padrão a ser aplicado
+        , triangle = Trianglify({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            variance: .7,
+            cell_size: randomSize,
+            x_colors: colors[randomColor]
+        });
+
+        // Aplica o Trianglify se o elemento existir
         if ( mainHeader ) mainHeader.setAttribute('style', 'background-image: url(' + triangle.png() + ')');
         if ( menuWrap ) menuWrap.setAttribute('style', 'background-image: url(' + triangle.png() + ')');
     },
 
+    /**
+     * hamburguerActions
+     * @access public
+     * @desc Abre e fecha o menu lateral
+     * @return {void}
+     */
     hamburguerActions: function( action ) {
         "use strict";
 
@@ -63,6 +85,12 @@ var Kopplin = {
         }
     },
 
+    /**
+     * hamburguerVisibility
+     * @access public
+     * @desc Trata a visibildade do hamburguer em determinada condição
+     * @return {void}
+     */
     hamburguerVisibility: function() {
         "use strict";
 
@@ -81,13 +109,40 @@ var Kopplin = {
         });
     },
 
+    /**
+     * clipboards
+     * @access public
+     * @desc Adiciona os clipboards nos elementos de código da página
+     * @return {void}
+     */
+    clipboards: function() {
+        //     new Clipboard('.clipboard');
+        //
+        //     var numberOfPre = $('pre').length;
+        //
+        //     if (numberOfPre > 0) {
+        //         for(var id = 0; id < numberOfPre; id++) {
+        //             var selectedPre = $('pre')[id];
+        //
+        //             html = '';
+        //             html += '<button class="clipboard" data-clipboard-target="#preId' + id +'"><span class="btn-clipboard icon-paste" alt="copiar"></span></button>';
+        //             $(selectedPre).attr('id', 'preId' + id).after(html);
+        //         }
+        //     }
+    },
+
+    /**
+     * events
+     * @access public
+     * @desc Adiciona os eventos de botão e de scroll ao app
+     * @return {void}
+     */
     events: function() {
         "use strict";
 
-        console.log("events");
-
         var hamburguerOpen = document.querySelector("#hamburguer__open")
-            , hamburguerClose = document.querySelector("#hamburguer__close");
+            , hamburguerClose = document.querySelector("#hamburguer__close")
+            , scrollBtn = document.querySelector("[data-scroll]");
 
         if ( hamburguerOpen && hamburguerClose ) {
             hamburguerOpen.addEventListener("click", function() {
@@ -105,7 +160,8 @@ var Kopplin = {
             Kopplin.hamburguerVisibility();
         }
 
-        console.log("fim // events");
+        if (scrollBtn) smoothScroll.init();
+
     }
 
 };
@@ -114,22 +170,4 @@ var Kopplin = {
     "use strict";
 
     Kopplin.init();
-
 })();
-
-// $(document).ready(function() {
-//
-//     new Clipboard('.clipboard');
-//
-//     var numberOfPre = $('pre').length;
-//
-//     if (numberOfPre > 0) {
-//         for(var id = 0; id < numberOfPre; id++) {
-//             var selectedPre = $('pre')[id];
-//
-//             html = '';
-//             html += '<button class="clipboard" data-clipboard-target="#preId' + id +'"><span class="btn-clipboard icon-paste" alt="copiar"></span></button>';
-//             $(selectedPre).attr('id', 'preId' + id).after(html);
-//         }
-//     }
-// });
